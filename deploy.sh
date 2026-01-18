4ccde2150202c55b73aead26b9f7e69a14700455
Lokeshpatil10 <lokeshsp26@gmail.com>
2026-01-18 17:52:01 +0530

    run on port 3000

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
-cd 100devs-ci-cd
+#!/bin/bash
+set -e
+
+cd /home/ubuntu/100xdevs-ci-cd
+
 git pull origin main
 npm install
 npm run build
-npm run start
+
+pm2 restart 100xdevs-ci-cd || pm2 start npm --name "100xdevs-ci-cd" -- start
