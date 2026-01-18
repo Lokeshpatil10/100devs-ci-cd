1b7ccf4d45ebb1808c9d55e510f57366c9aabd82
Lokeshpatil10 <lokeshsp26@gmail.com>
2026-01-18 17:54:28 +0530

    path changed

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-cd 100xdevs-ci-cd
+cd 100devs-ci-cd
 
 git pull origin main
 npm install
 npm run build
 
-pm2 restart 100xdevs-ci-cd || pm2 start npm --name "100xdevs-ci-cd" -- start
+pm2 restart 100devs-ci-cd || pm2 start npm --name "100devs-ci-cd" -- start
